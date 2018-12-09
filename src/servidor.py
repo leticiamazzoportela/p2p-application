cc64098c48f7fb89b91b97d81f757740349b3dda
Elaine Sangali <e.nani92@gmail.com>
2018-12-09 11:44:26 -0200

    criando função de upload

--- src/servidor.py
+++ src/servidor.py
@@ -5,6 +5,9 @@ import threading
 import Usuario
 
 class Server(threading.Thread):
+
+    lista = []
+
     def run(self):
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         print("Servidor iniciado...\n")
@@ -15,12 +18,19 @@ class Server(threading.Thread):
         self.sock.listen(1)
         print("Servidor rodando na porta %d\n" %port)
 
-        (clientname,address) = self.sock.accept()
+        (clientname, address) = self.sock.accept()
         print("Conectado em %s\n" % str(address)) 
         while 1:
-            chunk = clientname.recv(4096)            
-            print(address, ':', chunk)
-            
+            data = clientname.recv(4096)     
+            msg = data.decode()
+            if isinstance(msg, Usuario):
+                uploadArquivo(msg)
+                      
+    def uploadArquivo():
+
+
+
+
 
 if __name__=='__main__':
     srv = Server()
